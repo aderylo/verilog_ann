4c5eca496447ed5cab372e5444b5895c34a893ef
Adam Deryło <>
2024-03-14 12:07:15 +0100

    Modify relu to calculate activations for matrices

--- src/activation/relu.sv
+++ src/activation/relu.sv
@@ -1,9 +1,10 @@
 interface activation_function_if #(
-    parameter int VECTOR_LEN = 3
+    parameter int VECTOR_LEN = 3,
+    parameter int COUNT = 1
 );
 
-  logic signed [31:0] data_in [VECTOR_LEN];
-  logic signed [31:0] data_out[VECTOR_LEN];
+  logic signed [31:0] data_in [COUNT][VECTOR_LEN];
+  logic signed [31:0] data_out[COUNT][VECTOR_LEN];
 
 endinterface
 
@@ -12,12 +13,14 @@ module relu (
 );
 
   always_comb begin
-    for (int i = 0; i < intf.VECTOR_LEN; i++) begin
-      // check sign bit if 1 then is negative (IEEE 754)
-      if (intf.data_in[i][31] == 1) begin
-        intf.data_out[i] = 0;
-      end else begin
-        intf.data_out[i] = intf.data_in[i];
+    for (int i = 0; i < intf.COUNT; i++) begin
+      for (int j = 0; j < intf.VECTOR_LEN; j++) begin
+        // check sign bit if 1 then is negative (IEEE 754)
+        if (intf.data_in[i][j][31] == 1) begin
+          intf.data_out[i][j] = 0;
+        end else begin
+          intf.data_out[i][j] = intf.data_in[i][j];
+        end
       end
     end
   end
--- src/activation/relu_tb.sv
+++ src/activation/relu_tb.sv
@@ -3,38 +3,47 @@
 module relu_tb;
 
   parameter int VECTOR_LEN = 4;
+  parameter int COUNT = 1;
   parameter int CLK_PERIOD = 20;  // 50 Mhz
 
-  activation_function_if #(.VECTOR_LEN(VECTOR_LEN)) intf ();
+  activation_function_if #(
+      .VECTOR_LEN(VECTOR_LEN),
+      .COUNT(COUNT)
+  ) intf ();
   relu relu_dut (.intf(intf));
 
   // variables for testing
   bit good = 1;
-  int i = 0;
-  shortreal test_in[VECTOR_LEN] = '{-1.0, 1.0, -2.0, 2.0};
-  shortreal grandtruth[VECTOR_LEN] = '{0.0, 1.0, 0.0, 2.0};
-  shortreal test_out[VECTOR_LEN];
+  int i = 0, j = 0;
+  shortreal test_in[COUNT][VECTOR_LEN] = '{'{-1.0, 1.0, -2.0, 2.0}};
+  shortreal grandtruth[COUNT][VECTOR_LEN] = '{'{0.0, 1.0, 0.0, 2.0}};
+  shortreal test_out[COUNT][VECTOR_LEN];
 
   initial begin
     // load data
-    for (i = 0; i < VECTOR_LEN; i++) begin
-      intf.data_in[i] = $shortrealtobits(test_in[i]);
+    for (i = 0; i < COUNT; i++) begin
+      for (j = 0; j < VECTOR_LEN; j++) begin
+        intf.data_in[i][j] = $shortrealtobits(test_in[i][j]);
+      end
     end
 
-    #(CLK_PERIOD * 6);
+    #(CLK_PERIOD * 1);
 
     // get data out
-    for (i = 0; i < VECTOR_LEN; i++) begin
-      test_out[i] = $bitstoshortreal(intf.data_out[i]);
+    for (i = 0; i < COUNT; i++) begin
+      for (j = 0; j < VECTOR_LEN; j++) begin
+        test_out[i][j] = $bitstoshortreal(intf.data_out[i][j]);
+      end
     end
 
     // verify
     good = 1;
-    for (i = 0; i < VECTOR_LEN; i++) begin
-      $display("%f", test_out[i]);
-      good &= (grandtruth[i] == test_out[i]);
+    for (i = 0; i < COUNT; i++) begin
+      for (j = 0; j < VECTOR_LEN; j++) begin
+        $display("%f", test_out[i][j]);
+        good &= (grandtruth[i][j] == test_out[i][j]);
+      end
     end
-
     assert (good) $display("PASSED");
     else $display("FAILED");
 
